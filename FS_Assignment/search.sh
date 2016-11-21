90265a0de97d0252f302e9a113fc16a1ce2dc874
Raul Arrabales <raul.arrabales@gmail.com>
2016-11-21 20:03:57 +0100

    Update search.sh

--- FS_Assignment/search.sh
+++ FS_Assignment/search.sh
@@ -1,5 +1,22 @@
-#! /bin/bash 
+#! /bin/bash
 
-# U-tad - Operating Systems
-# File Systems Assignment 2016-17
+# Number of parameters
+params=3 
 
+echo Program $0 started with $# parameters... 
+
+case "$#" in 
+
+$params) # Right number of parameters
+
+  echo Using $0 with $# parameters: [$1] [$2] [$3]
+
+;;
+
+*)  # Incorrect number of parameters
+
+  echo ERROR: Incorrect number of parameters. 
+  echo Please use: $0 [directory] [criteria] [action]
+
+;;
+esac
