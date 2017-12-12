1f7406c2ccb071579f026ec667d446969cb90664
Raul Arrabales <raul.arrabales@gmail.com>
2017-12-12 20:35:21 +0100

    Update fetch.sh

--- FS_Assignment/fetch.sh
+++ FS_Assignment/fetch.sh
@@ -6,6 +6,14 @@ echo Usando el directorio $1
 
 
 for fichero in "$1"/*; do
+
+# -a FILE        True if file exists.
+# -e FILE        True if file exists.
+# -f FILE        True if file exists and is a regular file.
+# -r FILE        True if file is readable by you.
+# -s FILE        True if file exists and is not empty.
+# -w FILE        True if the file is writable by you.
+
 if [[ -r "$fichero" && -f "$fichero" ]] 
 then
     lista+=($fichero) 
